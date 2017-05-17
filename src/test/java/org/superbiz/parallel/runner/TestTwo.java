5021690ce21350d03be3e4632afbcc53ab75ed94
Alex Soto <asotobu@gmail.com>
2017-05-17 11:02:22 +0200

    Initial commit

---
+++ src/test/java/org/superbiz/parallel/runner/TestTwo.java
@@ -0,0 +1,23 @@
+package org.superbiz.parallel.runner;
+
+import java.util.concurrent.TimeUnit;
+import org.arquillian.cube.HostPort;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class TestTwo {
+
+
+    @HostPort(containerName = "redis*", value = 6379)
+    private int portBinding;
+
+
+    @Test
+    public void should_print_port_binding() throws InterruptedException {
+        System.out.println(TestOne.class.getCanonicalName() + " - " + portBinding);
+        TimeUnit.SECONDS.sleep(4);
+    }
+
+}
